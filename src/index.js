66ff617e3c6cc9d1e555b778485325e8c50c7bbd
azu <azuciao@gmail.com>
2016-06-02 14:29:31 +0900

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@
 "use strict";
 import React from 'react';
 import ReactDOM from "react-dom";
-import SizeMe from "react-sizeme";
 import MyWindow from "./MyWindow";
 ReactDOM.render(<MyWindow/>, document.getElementById("js-app"));
